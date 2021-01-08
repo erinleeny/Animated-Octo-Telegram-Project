f3d37a34ea3c1bff78cb25f6138225bcd1e3f32e
Erin Lee <erin.lee.ny@gmail.com>
2021-01-07 23:25:50 -0500

    editing, displaying, navigating

--- app/app.py
+++ app/app.py
@@ -1,43 +1,36 @@
 #Team Lach On (Maddy Andersen, Dean Carey, Kelly Huang, Erin Lee)
 #SoftDev -- Rona Ed.
-#Due 01-04-2021
+#P0 - Da Art of Storytellin'(Pt.2)
+#Due 01-08-2021
 
 import sqlite3
 from flask import Flask, render_template, session
 from flask import request, redirect, url_for
 import os
 import random
-from db_builder import create_user, authenticate_user, create_blog, create_entry, list_blogs, list_entries
+from db_builder import create_user, authenticate_user, create_blog, create_entry, list_blogs, list_entries, update_blog, update_entry, list_users
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32) #set up session secret key
-#c = db.cursor() #facilitate db ops
-user = "lacher"
-passw = "pass"
 
-@app.route("/")
-def test_tmplt():
+@app.route("/", methods=['POST', 'GET'])
+def home():
     if "username" in session:
-        return render_template('profile.html', user_id=session['user_id'], fName= session["first_name"], lName= session["last_name"], name=session['username'], password=session['password'],method=session['method'], blogs=list_blogs(session["user_id"]))
+        return render_template('profile.html', user_id=session['user_id'], fName= session["first_name"], lName= session["last_name"], name=session['username'], password=session['password'], blogs=list_blogs(session["user_id"]))
+    if "blog_id" in session:
+        session.pop("blog_id")
+        session.pop("blog_name")
+        session.pop("blog_description")
     return render_template('login.html', message="")
-
 @app.route("/register", methods=['POST','GET'])
 def register():
     return render_template('register.html')
-
 @app.route("/create_user", methods=['POST','GET'])
 def user():
-    method=request.method
-    if method == "POST":
-        first_name=request.form["first_name"]
-        last_name=request.form["last_name"]
-        username=request.form["username"]
-        password=request.form["password"]
-    else:
-        first_name=request.args['first_name']
-        last_name=request.args['last_name']
-        username=request.args['username']
-        password=request.args['password']
+    first_name=request.form["first_name"]
+    last_name=request.form["last_name"]
+    username=request.form["username"]
+    password=request.form["password"]
     db = sqlite3.connect("blog")
     c = db.cursor()
     c.execute("SELECT * FROM users WHERE username = '" + username + "'") #check to see whether user already exists
@@ -52,46 +45,31 @@ def user():
         ## todo: direct to a different error page that takes you to sign up page
     session["username"] = username
     session["password"] = password
-    session["method"] = method
     session["user_id"] = user_id
     session["first_name"] = first_name
     session["last_name"] = last_name
-    return render_template('profile.html', fName=first_name, lName=last_name, name=username, password=password, user_id=user_id, method=method)
-
+    return render_template('profile.html', fName=first_name, lName=last_name, name=username, password=password, user_id=user_id)
+# blog methods
 @app.route(("/register_blog"), methods=["POST", "GET"])
 def blog_register():
     return render_template('create_blog.html')
-
-@app.route(("/create_entry"), methods=["POST", "GET"])
-def entry_register():
-    return render_template('create_entry.html')
-@app.route(("/save_entry"), methods=["POST", "GET"])
-def entry_save():
-    method=request.method
-    if method == "POST":
-        entry_title=request.form["entry_title"]
-        content=request.form['content']
-    else:
-        entry_title=request.args['entry_title']
-        content=request.args['content']
-    entry_id = str(random.randint(0,10000000))
-    create_entry(entry_id, session["blog_id"], entry_title, content, "2021-01-06")
-    return render_template('blog.html', blog_id=session["blog_id"], username=session["username"], name=session["blog_name"])
-
+@app.route(("/edit_blog"), methods=["POST", "GET"])
+def blog_edit():
+    return render_template('edit_blog.html', description=session["blog_description"], name=session["blog_name"])
+@app.route(("/save_blog"), methods=["POST", "GET"])
+def blog_save():
+    name=request.form["name"]
+    description=request.form['description']
+    update_blog(session["blog_id"], name, description)
+    return redirect(url_for('show_existing_blog'))
 @app.route(("/create_blog"), methods=["POST", "GET"])
 def blog():
     #todo: some way to catch error if blog is blank or something
-    method=request.method
-    if method == "POST":
-        name=request.form["blog_name"]
-        description=request.form['description']
-    else:
-        name=request.args['name']
-        description=request.args['description']
+    name=request.form["name"]
+    description=request.form['description']
     blog_id = str(random.randint(0,10000000))
-    create_blog(blog_id, name, description, session["user_id"], "2021-01-06")
+    create_blog(blog_id, name, description, session["user_id"])
     return redirect(url_for('show_blog', blog_id=blog_id, name=name, description=description))
-
 @app.route(("/show_blog"), methods=["POST", "GET"])
 def show_blog():
     blog_id = request.args["blog_id"]
@@ -101,15 +79,13 @@ def show_blog():
     session["blog_name"] = name
     session["blog_description"] = description
     print(session["blog_id"])
-    return render_template('blog.html', blog_id=blog_id, name=name, description=description, username=session["username"])
-
+    return redirect(url_for('show_existing_blog'))
 @app.route(("/show_existing_blog"), methods=["POST", "GET"])
 def show_existing_blog():
-    method=request.method
-    if method == "POST":
+    if "id" in request.form:
         blog_id=request.form["id"]
     else:
-        blog_id=request.args["id"]
+        blog_id = session["blog_id"]
     db = sqlite3.connect("blog")
     c = db.cursor()
     c.execute("SELECT name, description FROM blogs WHERE blog_id=" + blog_id)
@@ -117,47 +93,63 @@ def show_existing_blog():
     session["blog_id"] = blog_id
     session["blog_name"] = data[0]
     session["blog_description"] = data[1]
+    return render_template('blog.html', blog_id=blog_id, name=data[0], description=data[1], username=session["username"], entries=list_entries(session["blog_id"]))
 
-    c.execute("SELECT title, content FROM entries WHERE blog_id=" + blog_id)
+# entry methods
+@app.route(("/create_entry"), methods=["POST", "GET"])
+def entry_register():
+    return render_template('create_entry.html')
+@app.route(("/update_entry"), methods=["POST", "GET"])
+def entry_update():
+    title=request.form["entry_title"]
+    content=request.form['content']
+    update_entry(session["entry_id"], title, content)
+    return redirect(url_for('show_existing_blog'))
+@app.route(("/edit_entry"), methods=["POST", "GET"])
+def entry_edit():
+    entry_id=request.form["id"]
+    db = sqlite3.connect("blog")
+    c = db.cursor()
+    c.execute("SELECT title, content FROM entries WHERE entry_id=" + entry_id)
     data_entries = c.fetchone()
-    check = c.fetchall()
-    if len(check) == 0:
-        session["blog_id"] = blog_id
-        return render_template('blog.html', blog_id=blog_id, name=data[0], description=data[1], username=session["username"], entries=list_entries(session["blog_id"]))
+    session["entry_id"]=entry_id
+    return render_template('edit_entry.html', title=data_entries[0], content=data_entries[1])
+@app.route(("/save_entry"), methods=["POST", "GET"])
+def entry_save():
+    entry_title=request.form["entry_title"]
+    content=request.form['content']
+    entry_id = str(random.randint(0,10000000))
+    create_entry(entry_id, session["blog_id"], entry_title, content, "2021-01-06")
+    return redirect(url_for('show_existing_blog'))
+
+@app.route("/all", methods=['POST', 'GET'])
+def all():
+    if "id" in request.form:
+        user_id=request.form["id"]
     else:
-        session["blog_id"] = blog_id
-        session["entry_title"] = data_entries[0]
-        session["entry_content"] = data_entries[1]
-        return render_template('blog.html', blog_id=blog_id, name=data[0], description=data[1], title=data_entries[0], content=data_entries[1], username=session["username"], entries=list_entries(session["blog_id"]))
+        user_id = session["user_id"]
+    db = sqlite3.connect("blog")
+    c = db.cursor()
+    c.execute("SELECT user_id, username, first_name, last_name FROM users WHERE user_id !=" + str(user_id))
+    data = c.fetchone()
+    print(data[1])
+    print(data[2])
+    print(data[3])
+    return render_template('masterlist.html', user_id = session['user_id'], users=list_users())
 
 @app.route("/logout", methods=['POST', 'GET'])
 def logout():
     session.pop("username")
     session.pop("password")
-    session.pop("method")
+    session.pop("first_name")
+    session.pop("last_name")
+    session.pop("user_id")
     return render_template('login.html', message="You have been successfully logged out.")
 
 @app.route("/auth", methods=['POST', 'GET'])
 def authenticate():
-    method=request.method
-    if method == "POST":
-        username=request.form["username"]
-        password=request.form["password"]
-    else:
-        username=request.args['username']
-        password=request.args['password']
-
-    ## todo: authenticate against the actual table of users, not hard-coded
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request.method)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    print("hello")
+    username=request.form["username"]
+    password=request.form["password"]
     db = sqlite3.connect("blog")
     c = db.cursor()
     c.execute("SELECT user_id, first_name, last_name FROM users WHERE username= ? AND password = ? ", (username, password))
@@ -169,19 +161,12 @@ def authenticate():
         user_id = d[0]
         first_name = d[1]
         last_name = d[2]
-   # if(username != user):
-   #     if(password != passw): #both password and username wrong
-   #           return render_template('error.html', message="Incorrect username and password")
-   #     return render_template('error.html', message="Incorrect username")
-   # if(password != passw):
-   #     return render_template('error.html', message="Incorrect password")
     session["username"] = username
     session["password"] = password
-    session["method"] = method
     session["user_id"] = user_id
     session["first_name"] = first_name
     session["last_name"] = last_name
-    return render_template('profile.html', user_id=user_id, name=username, fName=first_name, lName=last_name, password=password, method=method,data=list_blogs(session["user_id"]), blogs=list_blogs(user_id))  #response to a form submission
+    return render_template('profile.html', user_id=user_id, name=username, fName=first_name, lName=last_name, password=password, data=list_blogs(session["user_id"]), blogs=list_blogs(user_id))  #response to a form submission
 
 if __name__ == "__main__":  # true if this file NOT imported
   app.debug = True        # enable auto-reload upon code change
--- app/db_builder.py
+++ app/db_builder.py
@@ -1,4 +1,7 @@
-#Team LachOn
+#Team Lach On (Maddy Andersen, Dean Carey, Kelly Huang, Erin Lee)
+#SoftDev -- Rona Ed.
+#P0 - Da Art of Storytellin'(Pt.2)
+#Due 01-08-2021
 
 import sqlite3
 import random
@@ -8,16 +11,14 @@ db = sqlite3.connect("blog")
 
 c = db.cursor() #facilitate db ops
 
-#c.execute("CREATE TABLE users(user_id INTEGER PRIMARY KEY, username TEXT, first_name TEXT, last_name TEXT, password TEXT)")
-#c.execute("CREATE TABLE blogs(blog_id INTEGER PRIMARY KEY, name TEXT, description TEXT, user_id INTEGER, creation_date TEXT)")
-#c.execute("CREATE TABLE entries(entry_id INTEGER PRIMARY KEY, blog_id INTEGER, title TEXT, content TEXT, creation_date TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY, username TEXT, first_name TEXT, last_name TEXT, password TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS blogs(blog_id INTEGER PRIMARY KEY, name TEXT, description TEXT, user_id INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS entries(entry_id INTEGER PRIMARY KEY, blog_id INTEGER, title TEXT, content TEXT)")
 #c.execute("INSERT INTO users VALUES(0, 'LachOn', 'Team', 'LachOn', 'tester')")
 
 def create_user(first_name, last_name, username, password, user_id):
 	db = sqlite3.connect("blog")
-
 	c = db.cursor() #facilitate db ops
-
 	c.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?)", (user_id, username, first_name, last_name, password))
 	db.commit() #save changes
 	db.close()
@@ -31,24 +32,19 @@ def authenticate_user(username, password):
 	db.close()
 	return result_set
 
-def create_blog(blog_id, name, description, user_id, creation_date):
+def create_blog(blog_id, name, description, user_id):
+    creation_date = "none"
     db = sqlite3.connect("blog")
-
     c = db.cursor() #facilitate db ops
-
     c.execute("INSERT INTO blogs VALUES(?, ?, ?, ?, ?)", (blog_id, name, description, user_id, creation_date))
-
     db.commit() #save changes
     db.close()
 
-
-def create_entry(entry_id, blog_id, title, content, creation_date):
+def create_entry(entry_id, blog_id, title, content):
+    creation_date = "none"
     db = sqlite3.connect("blog")
-
     c = db.cursor() #facilitate db ops
-
     c.execute("INSERT INTO entries VALUES(?, ?, ?, ?, ?)", (entry_id, blog_id, title, content, creation_date))
-
     db.commit() #save changes
     db.close()
 
@@ -71,15 +67,46 @@ def list_blogs(user_id):
 def list_entries(blog_id):
     db = sqlite3.connect("blog")
     c = db.cursor() #facilitate db ops
-    c.execute("SELECT blog_id, title, content FROM entries WHERE blog_id= " + str(blog_id))
+    c.execute("SELECT entry_id, title, content FROM entries WHERE blog_id= " + str(blog_id))
     data = c.fetchall()
     entries = []
     for d in data:
         entry = {}
-        entry["blog_id"]=d[0]
+        entry["entry_id"]=d[0]
         entry["title"]=d[1]
         entry["content"]=d[2]
         entries.append(entry)
     db.commit() #save changes
     db.close()
     return entries
+
+def update_blog(blog_id, name, description):
+    db = sqlite3.connect("blog")
+    c = db.cursor()
+    c.execute("UPDATE blogs SET name= ?, description = ? WHERE blog_id = ?", (name, description, blog_id))
+    db.commit() #save changes
+    db.close()
+
+def update_entry(entry_id, title, content):
+    db = sqlite3.connect("blog")
+    c = db.cursor()
+    c.execute("UPDATE entries SET title= ?, content = ? WHERE entry_id = ?", (title, content, entry_id))
+    db.commit() #save changes
+    db.close()
+
+def list_users():
+	db = sqlite3.connect("blog")
+	c = db.cursor() #facilitate db ops
+	c.execute("SELECT user_id, username, first_name, last_name FROM users")
+	data = c.fetchall()
+	users = []
+	for d in data:
+		user = {}
+		user["user_id"]=d[0]
+		user["username"]=d[1]
+		user["first_name"]=d[2]
+		user["last_name"]=d[3]
+		users.append(user)
+	db.commit() #save changes
+	db.close()
+	return users
